d1bf3c88d9748ffcd8699d8866c2e7bccaa53194
Antoine Willerval <wilate@yahoo.fr>
2021-08-24 11:03:36 +0200

    Revert "Update Plugin API to v25"

--- plugin.c
+++ plugin.c
@@ -30,7 +30,7 @@ static struct TS3Functions ts3Functions;
 #define _strcpy(dest, destSize, src) { strncpy(dest, src, destSize-1); (dest)[destSize-1] = '\0'; }
 #endif
 
-#define PLUGIN_API_VERSION 24
+#define PLUGIN_API_VERSION 21
 
 #define PATH_BUFSIZE 512
 #define COMMAND_BUFSIZE 128
